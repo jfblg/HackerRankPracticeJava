40351aad392c9dbdea958fd5a100f6f29eff8121
jfblg <inbox.jfblg@gmail.com>
2019-05-23 17:27:11 +0200

    add incomplete solution

--- src/main/java/Java1DArrayPart2.java
+++ src/main/java/Java1DArrayPart2.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+/**
+ * 4
+ * 5 3
+ * 0 0 0 0 0
+ * 6 5
+ * 0 0 0 1 1 1
+ * 6 3
+ * 0 0 1 1 1 0
+ * 3 1
+ * 0 1 0
+ *
+ * YES
+ * YES
+ * NO
+ * NO
+ */
 
 public class Java1DArrayPart2 {
+
+    public static boolean canMoveForward(int[] arr, int index, int step) {
+        if(index + step > arr.length -1)
+            return true;
+        else if(arr[index + step] == 0)
+            return true;
+        else if(step > arr.length - index)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean canWin(int leap, int[] game) {
+        int index = 0;
+
+        while(index <= game.length) {
+            if(canMoveForward(game, index, leap))
+                index += leap;
+            else if (canMoveForward(game, index, 1))
+                index++;
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+
+        while(q-- > 0) {
+            int n = in.nextInt();
+            int leap = in.nextInt();
+
+            int[] game = new int[n];
+            for(int i=0; i<n; i++) {
+                game[i] = in.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO");
+        }
+
+        in.close();
+    }
 }
--- src/main/java/JavaList.java
+++ src/main/java/JavaList.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class JavaList {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList<Integer> L = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            L.add(in.nextInt());
+        }
+        int Q = in.nextInt();
+        in.nextLine();
+        int index;
+        int value;
+
+        while(Q-- > 0) {
+            String operation = in.nextLine();
+            if(operation.equals("Insert")) {
+                index = in.nextInt();
+                value = in.nextInt();
+                in.nextLine();
+                L.add(index, value);
+
+            } else if (operation.equals("Delete")) {
+                index = Integer.parseInt(in.nextLine());
+                L.remove(index);
+            } else {
+                System.out.println("Invalid input!");
+            }
+        }
+        in.close();
+
+        ListIterator<Integer> listItr = L.listIterator();
+
+        while(listItr.hasNext()){
+            System.out.printf("%d", listItr.next());
+            if(listItr.hasNext()) {
+                System.out.printf(" ");
+            } else {
+                System.out.printf("\n");
+            }
+        }
+        System.out.println();
+    }
+
 }
