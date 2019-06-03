87867d655548638fdfa5da835584fe5221b75239
jfblg <inbox.jfblg@gmail.com>
2019-06-03 13:33:47 +0200

    add solution

--- src/main/java/JavaArraylist.java
+++ src/main/java/JavaArraylist.java
@@ -1,29 +1,38 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class JavaArraylist {
+
+    // TODO implement iterator
+    // TODO implement static function which prints the requests
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         List<List> listOfLists = new ArrayList<>();
 
         int n = Integer.parseInt(sc.nextLine());
+
         for(int i=0; i<n; i++){
             int d = sc.nextInt();
             List<Integer> listOfInt= new ArrayList<>();
-            while(d-- >= 0) {
+            for(int j=0; j<d; j++) {
                 listOfInt.add(sc.nextInt());
-
             }
-            sc.nextLine();
             listOfLists.add(listOfInt);
+            sc.nextLine();
         }
         int q = Integer.parseInt(sc.nextLine());
         for(int i=0; i<q; i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
+            try {
+                System.out.println(listOfLists.get(x-1).get(y-1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERROR!");
+            }
             sc.nextLine();
         }
-
         sc.close();
-
     }
 }
