125c02f20c854436abd80edcedf37ebba2aadca7
jfblg <inbox.jfblg@gmail.com>
2019-07-02 14:20:28 +0200

    solve solution

--- src/main/java/JavaDatatypes.java
+++ src/main/java/JavaDatatypes.java
@@ -10,26 +10,16 @@ public class JavaDatatypes {
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
 
-        int byteMin = -(int)Math.pow(2,8)/2;
-        int byteMax = (int)Math.pow(2,8)/2 -1;
-        int shortMin = -(int)Math.pow(2,16)/2;
-        int shortMax = (int)Math.pow(2,16)/2 -1;
-        int intMin = -(int)Math.pow(2,32)/2;
-        int intMax = (int)Math.pow(2,32)/2 -1;
-        long longMin = -(int)Math.pow(2,64)/2;
-        long longMax = (int)Math.pow(2,64)/2 -1;
-
-
         for(int i=0;i<t;i++)
         {
             try
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=byteMin && x<=byteMax)System.out.println("* byte");
-                if(x>=shortMin && x<=shortMax)System.out.println("* short");
-                if(x>=intMin && x<=intMax)System.out.println("* int");
-                if(x>=longMin && x<=longMax)System.out.println("* long");
+                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE)System.out.println("* byte");
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)System.out.println("* short");
+                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)System.out.println("* int");
+                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)System.out.println("* long");
                 //Complete the code
             }
             catch(Exception e)
